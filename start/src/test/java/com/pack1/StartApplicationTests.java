a9620b5f10994fcf2f1c91c63f66aa69ca11fb7d
cjx <cjx@qq.com>
2023-02-16 13:54:37 +0800

    6 commit

--- start/src/test/java/com/pack1/StartApplicationTests.java
+++ start/src/test/java/com/pack1/StartApplicationTests.java
@@ -12,6 +12,7 @@ class StartApplicationTests {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("master test");
     }
 
 }
