2e9a6571a11acf7bd2a06a782b2bd42ab52d3335
cjx <cjx@qq.com>
2023-02-16 15:12:34 +0800

    10 commit

---
+++ start1/src/main/java/com/pack1/start1/HelloResource.java
@@ -0,0 +1,11 @@
+package com.pack1.start1;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+public class HelloResource {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
